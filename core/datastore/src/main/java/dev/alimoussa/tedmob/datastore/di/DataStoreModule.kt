4e5fb1c549ea9d0fbc32fd3abc9b2759d0bbd8b2
Ali Moussa <ali999.1mousa@gmail.com>
2024-06-24 00:49:56 +0300

    injected using hilt userPreferencesDataStore

--- core/datastore/src/main/java/dev/alimoussa/tedmob/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/dev/alimoussa/tedmob/datastore/di/DataStoreModule.kt
@@ -1,2 +1,38 @@
 package dev.alimoussa.tedmob.datastore.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.alimoussa.tedmob.common.network.Dispatcher
+import dev.alimoussa.tedmob.common.network.TedmobDispatchers.IO
+import dev.alimoussa.tedmob.common.network.di.ApplicationScope
+import dev.alimoussa.tedmob.datastore.UserPreferencesSerializer
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import tedmob.UserPreferences
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun providesUserPreferencesDataStore(
+        @ApplicationContext context: Context,
+        @Dispatcher(IO) ioDispatcher: CoroutineDispatcher,
+        @ApplicationScope scope: CoroutineScope,
+        userPreferencesSerializer: UserPreferencesSerializer,
+    ): DataStore<UserPreferences> =
+        DataStoreFactory.create(
+            serializer = userPreferencesSerializer,
+            scope = CoroutineScope(scope.coroutineContext + ioDispatcher),
+        ) {
+            context.dataStoreFile("user_preferences.pb")
+        }
+}
